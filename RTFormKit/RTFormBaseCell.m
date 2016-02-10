e8da41c12bdfe50210abb8fe04806d993aa528b2
Aleksandar Vacić <av@radianttap.com>
2016-02-10 16:42:40 +0100

    Initial setup for the base cell

--- RTFormKit/RTFormBaseCell.m
+++ RTFormKit/RTFormBaseCell.m
@@ -22,9 +22,41 @@
 	return [[[self nib] instantiateWithOwner:nil options:nil] lastObject];
 }
 
+#pragma mark - Init
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+
+	self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+	if (!self) return nil;
+
+	//	let table view decide the background colors
+	self.backgroundColor = [UIColor clearColor];
+	self.contentView.backgroundColor = [UIColor clearColor];
+	self.selectionStyle = UITableViewCellSelectionStyleNone;
+	self.clipsToBounds = YES;
+
+	//	prepare row for self-sizing
+	NSDictionary *metrics = @{@"rowHeight": @(54)};
+	NSDictionary *vd = @{@"cv": self.contentView};
+	[self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[cv(rowHeight@749)]"
+																			 options:0
+																			 metrics:metrics
+																			   views:vd]];
+
+	[self commonInit];
+	return self;
+}
+
 - (void)awakeFromNib {
 	[super awakeFromNib];
 
+	[self commonInit];
 }
+
+- (void)commonInit {
+
+}
+
+#pragma mark -
 
 @end
