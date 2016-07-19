a50dce945b01f81b57ee700b24c7255edd92cc4c
Aleksandar Vacić <av@radianttap.com>
2016-07-19 21:14:37 +0200

    Finished NSCoding support

--- RTFormKit/RTFormDataItem.m
+++ RTFormKit/RTFormDataItem.m
@@ -39,6 +39,18 @@
 	newcopy.cellType = self.cellType;
 	newcopy.key = self.key;
 
+	newcopy.title = self.title;
+	newcopy.subtitle = self.subtitle;
+	newcopy.hint = self.hint;
+	newcopy.explanation = self.explanation;
+	newcopy.placeholder = self.placeholder;
+
+	newcopy.value = self.value;
+	newcopy.defaultValue = self.defaultValue;
+
+	newcopy.disabled = self.disabled;
+	newcopy.mandatory = self.mandatory;
+
 	return newcopy;
 }
 
@@ -46,6 +58,18 @@
 
 	[aCoder encodeObject:@(self.cellType) forKey:NSStringFromSelector(@selector(cellType))];
 	[aCoder encodeObject:self.key forKey:NSStringFromSelector(@selector(key))];
+
+	[aCoder encodeObject:self.title forKey:NSStringFromSelector(@selector(title))];
+	[aCoder encodeObject:self.subtitle forKey:NSStringFromSelector(@selector(subtitle))];
+	[aCoder encodeObject:self.hint forKey:NSStringFromSelector(@selector(hint))];
+	[aCoder encodeObject:self.explanation forKey:NSStringFromSelector(@selector(explanation))];
+	[aCoder encodeObject:self.placeholder forKey:NSStringFromSelector(@selector(placeholder))];
+
+	[aCoder encodeObject:self.value forKey:NSStringFromSelector(@selector(value))];
+	[aCoder encodeObject:self.defaultValue forKey:NSStringFromSelector(@selector(defaultValue))];
+
+	[aCoder encodeObject:@(self.isDisabled) forKey:NSStringFromSelector(@selector(isDisabled))];
+	[aCoder encodeObject:@(self.isMandatory) forKey:NSStringFromSelector(@selector(isMandatory))];
 }
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
@@ -55,6 +79,18 @@
 
 	_cellType = [[aDecoder decodeObjectForKey:NSStringFromSelector(@selector(cellType))] integerValue];
 	_key = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(key))];
+
+	_title = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(title))];
+	_subtitle = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(subtitle))];
+	_hint = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(hint))];
+	_explanation = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(explanation))];
+	_placeholder = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(placeholder))];
+
+	_value = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(value))];
+	_defaultValue = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(defaultValue))];
+
+	_disabled = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(isDisabled))];
+	_mandatory = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(isMandatory))];
 
 	return self;
 }
